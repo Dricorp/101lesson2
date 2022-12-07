7d47fcce2f0936acb2282ff72f081d95ad5c4e2b
Dricorp <113799720+Dricorp@users.noreply.github.com>
2022-12-07 10:25:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+let message = 'Hello';
+console.log(message);
+
+let n = 123;
+console.log(n);
+n = 12.345;
+console.log(n);
+
+alert(1 / 0);//infinity - математическая бесконечность
+alert(Infinity); // задана явно
+alert('не число' / 2); //математическая ошибка nan
+
+let str = "Selam aleykum cemaat";
+console.log(str);
+
+let str1 = 'Nasilsiniz?'
+console.log(str1);
+
+let str2 = `Boyle bence daha guzel ${str1};` //`Обратные кавычки позволяют встраивать переменные ${ str }`
+console.log(str2);
+
+let username = `Ahmed`;
+alert(`Selam aleykum, ${username} !`);
+username = 2 + 2;
+console.log(typeof (username));
+console.log(username);
+
+
+let isGreater = 4 > 1;
+console.log(isGreater);
+let example = 0 > 1;
+console.log(typeof (example));
+console.log(example);
+
+let age;
+console.log(age);
